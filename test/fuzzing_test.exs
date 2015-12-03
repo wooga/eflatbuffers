e8bbd696958db4457886ce54afee5bc2567672d6
Björn Reimer <git@bjrm.de>
2015-12-03 12:12:30 +0100

    generator: added chance to drop keys and set values to defaults

--- test/fuzzing_test.exs
+++ test/fuzzing_test.exs
@@ -2,12 +2,14 @@ defmodule EflatbuffersTest.Fuzz do
   use ExUnit.Case
   import TestHelpers
 
-  test "fuzz based on doge schemas" do        
-    fuzz_schema({:doge, :game_state})
+  test "fuzz based on doge schemas" do    
+    [:config, :game_state, :battle_log, :commands]
+    |> Enum.map(fn(type) -> {:doge, type} end)
+    |> Enum.each(fn(doge_type) -> IO.inspect("Testing schema: #{inspect(doge_type)}"); fuzz_schema(doge_type) end)
   end
 
   def fuzz_schema(schema_type) do
-    map = Eflatbuffers.Generator.generate_from_schema(load_schema(schema_type))
+    map = Eflatbuffers.Generator.map_from_schema(load_schema(schema_type))
     assert {:ok, fb} = Eflatbuffers.write_fb(map, load_schema(schema_type))
     assert {:ok, map_re} = Eflatbuffers.read_fb(:erlang.iolist_to_binary(fb), load_schema(schema_type))
 
