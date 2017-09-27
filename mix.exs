dfb202180d46eded35b26ccbda9167a232f483ee
odo <florian.odronitz@wooga.net>
2017-09-27 10:18:50 +0200

    preparing for open sourcing

--- mix.exs
+++ mix.exs
@@ -2,12 +2,25 @@ defmodule Eflatbuffers.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :eflatbuffers,
-     version: "0.0.1",
-     elixir: ">= 1.1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :eflatbuffers,
+      version: "0.1.0",
+      description: "Elixir/Erlang flatbuffers implementation",
+      package: package(),
+      elixir: ">= 1.1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()]
+  end
+
+  defp package() do
+    [
+      name: "eflatbuffers",
+      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Florian Odronitz"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/wooga/eflatbuffers"}
+    ]
   end
 
   # Configuration for the OTP application
